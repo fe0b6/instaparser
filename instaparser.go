a5a9e98f6c3e64f95106b70b9381b258a37f41c9
fe0b6 <spirit-dnk@yandex.ru>
2017-06-10 11:33:27 +0300

    Add files via upload

---
+++ instaparser.go
@@ -0,0 +1,123 @@
+package instaparser
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+var (
+	dataReg *regexp.Regexp
+)
+
+func init() {
+	dataReg = regexp.MustCompile(`>window._sharedData = (.*);</script>`)
+}
+
+func GetLikes(lnk string) (pi PostInfo, err error) {
+	// Читаем страницу
+	resp, err := http.Get(lnk)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	// Что-то пошло не так
+	if resp.StatusCode != 200 {
+		err = errors.New(resp.Status)
+		log.Println("[error]", resp.Status, resp.StatusCode)
+		return
+	}
+
+	// Читаем ответ
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	arr := dataReg.FindStringSubmatch(string(content))
+	if len(arr) == 0 {
+		err = errors.New("can't find json data")
+		return
+	}
+
+	var pd PostData
+	err = json.Unmarshal([]byte(arr[1]), &pd)
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	if len(pd.EntryData.PostPage) == 0 {
+		err = errors.New("bad json data")
+		return
+	}
+
+	pi.Likes = pd.EntryData.PostPage[0].Graphql.ShortcodeMedia.EdgeMediaPreviewLike.Count
+	pi.Comments = pd.EntryData.PostPage[0].Graphql.ShortcodeMedia.EdgeMediaToComment.Count
+	pi.Id = pd.EntryData.PostPage[0].Graphql.ShortcodeMedia.Id
+	pi.Published = pd.EntryData.PostPage[0].Graphql.ShortcodeMedia.TakenAtTimestamp
+
+	if len(pd.EntryData.PostPage[0].Graphql.ShortcodeMedia.EdgeMediaToCaption.Edges) > 0 {
+		pi.Text = pd.EntryData.PostPage[0].Graphql.ShortcodeMedia.EdgeMediaToCaption.Edges[0].Node.Text
+	}
+
+	pi.Owner = pd.EntryData.PostPage[0].Graphql.ShortcodeMedia.Owner
+
+	return
+}
+
+func GetProfile(lnk string) (pi ProfileInfo, err error) {
+	// Читаем страницу
+	resp, err := http.Get(lnk)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	// Что-то пошло не так
+	if resp.StatusCode != 200 {
+		err = errors.New(resp.Status)
+		log.Println("[error]", resp.Status, resp.StatusCode)
+		return
+	}
+
+	// Читаем ответ
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	arr := dataReg.FindStringSubmatch(string(content))
+	if len(arr) == 0 {
+		err = errors.New("can't find json data")
+		return
+	}
+
+	var pd ProfileData
+	err = json.Unmarshal([]byte(arr[1]), &pd)
+	if err != nil {
+		log.Println("[error]", err)
+		return
+	}
+
+	if len(pd.EntryData.ProfilePage) == 0 {
+		err = errors.New("bad json data")
+		return
+	}
+
+	pi.FollowedBy = pd.EntryData.ProfilePage[0].User.FollowedBy.Count
+
+	return
+}
---
+++ type.go
@@ -0,0 +1,76 @@
+package instaparser
+
+type PostInfo struct {
+	Id        string
+	Published int
+	Likes     int
+	Comments  int
+	Text      string
+	Owner     PostDataOwner
+}
+
+type PostData struct {
+	EntryData PostDataEntryData `json:"entry_data"`
+}
+
+type PostDataEntryData struct {
+	PostPage []PostDataPostPage `json:"PostPage"`
+}
+
+type PostDataPostPage struct {
+	Graphql PostDataGraphql `json:"graphql"`
+}
+
+type PostDataGraphql struct {
+	ShortcodeMedia PostDataShortcodeMedia `json:"shortcode_media"`
+}
+
+type PostDataShortcodeMedia struct {
+	Id                   string                     `json:"id"`
+	TakenAtTimestamp     int                        `json:"taken_at_timestamp"`
+	EdgeMediaToCaption   PostDataEdgeMediaToCaption `json:"edge_media_to_caption"`
+	EdgeMediaToComment   DataCount                  `json:"edge_media_to_comment"`
+	EdgeMediaPreviewLike DataCount                  `json:"edge_media_preview_like"`
+	Owner                PostDataOwner              `json:"owner"`
+}
+
+type PostDataEdgeMediaToCaption struct {
+	Edges []PostDataEdges `json:"edges"`
+}
+
+type PostDataEdges struct {
+	Node PostDataEdgesNode `json:"node"`
+}
+
+type PostDataEdgesNode struct {
+	Text string `json:"text"`
+}
+
+type PostDataOwner struct {
+	Id       string `json:"id"`
+	Username string `json:"username"`
+}
+
+type ProfileInfo struct {
+	FollowedBy int
+}
+
+type ProfileData struct {
+	EntryData ProfileDataEntryData `json:"entry_data"`
+}
+
+type ProfileDataEntryData struct {
+	ProfilePage []ProfileDataProfilePage `json:"ProfilePage"`
+}
+
+type ProfileDataProfilePage struct {
+	User ProfileDataUser `json:"user"`
+}
+
+type ProfileDataUser struct {
+	FollowedBy DataCount `json:"followed_by"`
+}
+
+type DataCount struct {
+	Count int `json:"count"`
+}
